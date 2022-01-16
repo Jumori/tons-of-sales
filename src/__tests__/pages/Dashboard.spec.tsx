5c08cd2ec8041486555d9114466472e770526eee
Juliana Morikoshi <juliana@suthub.com>
2022-01-16 02:53:26 -0300

    test(dashboard): removing product

--- src/__tests__/pages/Dashboard.spec.tsx
+++ src/__tests__/pages/Dashboard.spec.tsx
@@ -132,4 +132,66 @@ describe('Dashboard', () => {
     expect(addToCart).toHaveReturnedWith(1);
     expect(getByTestId('remove-from-cart-1')).toBeTruthy();
   });
+
+  it('should be able to remove item from cart', async () => {
+    const { addToCart, removeFromCart } = useCart();
+
+    const products = {
+      products: [
+        {
+          id: '1',
+          title: 'Produto 01',
+          image_url: 'https://via.placeholder.com/500',
+          price: 10,
+        },
+        {
+          id: '2',
+          title: 'Produto 02',
+          image_url: 'https://via.placeholder.com/500',
+          price: 20,
+        },
+        {
+          id: '3',
+          title: 'Produto 03',
+          image_url: 'https://via.placeholder.com/500',
+          price: 30,
+        },
+        {
+          id: '4',
+          title: 'Produto 04',
+          image_url: 'https://via.placeholder.com/500',
+          price: 40,
+        },
+      ],
+    };
+
+    apiMock
+      .onGet(
+        'https://gist.githubusercontent.com/Jumori/c3451ba65a5027470122a81354718193/raw/tonsofsales-server.json',
+      )
+      .reply(200, products);
+
+    const { getByTestId, queryByTestId } = render(<Dashboard />);
+
+    await waitFor(() => expect(getByTestId('add-to-cart-1')).toBeTruthy(), {
+      timeout: 200,
+    });
+
+    await act(async () => {
+      await fireEvent.press(getByTestId('add-to-cart-1'));
+    });
+
+    expect(addToCart).toHaveBeenCalledWith({
+      ...products.products[0],
+      quantity: 0,
+    });
+    expect(addToCart).toHaveReturnedWith(1);
+    expect(getByTestId('remove-from-cart-1')).toBeTruthy();
+
+    await act(async () => {
+      await fireEvent.press(getByTestId('remove-from-cart-1'));
+    });
+    expect(removeFromCart).toHaveReturnedWith(0);
+    expect(queryByTestId('remove-from-cart-1')).toBeNull();
+  });
 });
